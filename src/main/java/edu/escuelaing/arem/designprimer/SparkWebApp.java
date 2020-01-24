41f9849438badb03ecb9589b9f7254a8201fe53b
NicoCarde <nicolas.cardenas@mail.escuelaing.edu.co>
2020-01-24 13:01:10 -0500

    Calculator

--- src/main/java/edu/escuelaing/arem/designprimer/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arem/designprimer/SparkWebApp.java
@@ -9,11 +9,17 @@ public class SparkWebApp {
 
     public static void main(String[] args) {
         port(getPort());
-        get("/hello", (req, res) -> "Hello Heroku");
+        get("/", (req, res) -> "Hello Heroku");
+        get("/hello/:name", (req, res) -> {
+            return "Hello: " + req.params(":name");
+        });
+        get("/calc", (req, res) -> {
+            return "";
+        }));
     }
     static int getPort() {
         if (System.getenv("PORT") != null) {
-        return Integer.parseInt(System.getenv("PORT"));
+            return Integer.parseInt(System.getenv("PORT"));
         }
         return 4567; //returns default port if heroku-port isn't set (i.e. on localhost)
     }
